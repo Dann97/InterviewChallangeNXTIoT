33b7c9605b76233168b0854597ee7b61eaa7386f
Juan Daniel Juarez <isc.danieljuarez@gmail.com>
2021-06-28 21:28:34 -0500

    Creando el login y agregando estilos con validaciones

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # interviewchallenge
-
+```
+Registrar diferentes usuarios únicamente con correo y contraseña y puedan crear su lista de pendientes de manera individual.
+```
 ## Project setup
 ```
 npm install
--- src/main.js
+++ src/main.js
@@ -1,9 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 Vue.config.productionTip = false
+Vue.use(BootstrapVue)
+Vue.use(BootstrapVueIcons)
+Vue.use(VueAxios, axios)
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 new Vue({
   router,
   render: h => h(App)
